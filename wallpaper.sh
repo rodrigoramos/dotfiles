2dfda28546a180627286bc705c4340a593971ca8
Rodrigo Ramos Silva <rramos.silva@gmail.com>
2019-11-26 18:56:01 -0300

    Wallpapers 2

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,10 +2,13 @@
 
 while [ : ] 
 do
-  wget -O /tmp/wallpaper.jpg https://unsplash.it/2560/1440/?random
-  gsettings set org.gnome.desktop.background picture-uri file:///tmp/wallpaper.jpg
+  wget -O /tmp/wallpaper-remoto.jpg https://unsplash.it/2560/1440/?random
+  gsettings set org.gnome.desktop.background picture-uri file:///tmp/wallpaper-remoto.jpg
 
-  /usr/bin/feh --no-fehbg --bg-scale /tmp/wallpaper.jpg 
+  file=$(ls ~/Imagens/wallpapers | sort -R | tail -1)
+  cp ~/Imagens/wallpapers/$file /tmp/wallpaper-local
+
+  /usr/bin/feh --no-fehbg --bg-scale /tmp/wallpaper-remoto.jpg /tmp/wallpaper-local
   
   sleep 10m
 done
