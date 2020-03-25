f2ca656c719ea1c189bba952cef483941708e69e
Rodrigo Ramos <rodrigo.ramos@lambda3.com.br>
2020-03-25 17:08:09 -0300

    Corrige tamanho possível para id

--- keyboard_layout_device.sh
+++ keyboard_layout_device.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 deviceName="AKKO 3068BT-1 Keyboard"
-deviceInputId=$(xinput -list | grep -oP "(?<=$deviceName\s{$[41 - ${#deviceName}]}id=)[0-9]{2}")
+deviceInputId=$(xinput -list | grep -oP "(?<=$deviceName\s{$[41 - ${#deviceName}]}id=)[0-9]{1,2}")
 
 setxkbmap -device $deviceInputId -layout us_intl
