15459f8c512a29d2cd93eafa23fe9a52dc70928d
Rodrigo Ramos <rodrigo.ramos@lambda3.com.br>
2021-03-30 11:24:14 -0300

    Move wallpaper script

---
+++ bin/wallpaper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while [ : ] 
+do
+  # wget -O /tmp/wallpaper-remoto.jpg https://picsum.photos/3840/2160?random
+
+  #file=$(ls ~/Imagens/wallpapers | sort -R | tail -1)
+  #cp ~/Imagens/wallpapers/$file /tmp/wallpaper-local
+
+
+  # Unsplash - API
+  # wallpaper_url=$(curl https://api.unsplash.com/photos/random?client_id=$UNSPLASH_CLIENT_ID\&orientation=landscape | jq '.urls.raw' | tr -d '"')
+  # wallpaper_url=$wallpaper_url\&w=3840\&h=2160\&fit=fill\&fill=blur
+
+  wget -O /tmp/wallpaper-remoto.jpg https://source.unsplash.com/random/3840x2160
+
+  killall swaybg
+  swaybg -m fit -i /tmp/wallpaper-remoto.jpg &
+  
+  sleep 10m
+ done
