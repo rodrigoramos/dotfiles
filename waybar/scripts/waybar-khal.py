b9df477a734094aa264a95d5024c676bb4123dae
Rodrigo Ramos Silva <rodrigo.silva@pravaler.com.br>
2023-07-28 11:31:17 -0300

    Backup

--- waybar/scripts/waybar-khal.py
+++ waybar/scripts/waybar-khal.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import subprocess
 import datetime
@@ -11,7 +11,7 @@ data = {}
 today = datetime.date.today().strftime("%d/%/%Y")
 
 until_date = (datetime.date.today() +
-             datetime.timedelta(days=4)).strftime("%d/%m/%Y")
+          datetime.timedelta(days=4)).strftime("%d/%m/%Y")
 
 output = subprocess.check_output("khal list now " + until_date + " --format '{start-time}-{end-time} {title}'", shell=True)
 output = output.decode("utf-8")
@@ -19,10 +19,11 @@ output = output.decode("utf-8")
 next_event_match = re.search("[0-9][0-9]:[0-9][0-9]-[0-9][0-9].+", output)
 
 if next_event_match == None:
-    data['text'] = ""
+ data['text'] = ""
 else:
-    data['text'] = " " + next_event_match.group()
+ data['text'] = "  " + next_event_match.group()
 
 data['tooltip'] = output
 
 print(json.dumps(data))
+
