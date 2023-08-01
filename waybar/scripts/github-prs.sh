62936433b353b6207b8d42d1f4cde66668f8f765
Rodrigo Ramos Silva <rodrigo.silva@pravaler.com.br>
2023-08-01 18:45:53 -0300

    Fix GH status and Khal Next event

--- waybar/scripts/github-prs.sh
+++ waybar/scripts/github-prs.sh
@@ -16,11 +16,12 @@ if [ "$pr_review_requests_count" == "0" ]; then
   pr_review_requests="It's all good ^_^"
   alt="nothingtodo"
 else
-  pr_review_requests="$(echo $github_status_output | grep -oE PRAVALER\/\[^#\]+#\[0-9\]\{1,4\}\[^│]+)"
+  # pr_review_requests="$(echo $github_status_output | grep -oE PRAVALER\/\[^#\]+#\[0-9\]\{1,4\}\[^│]+)"
+  pr_review_requests="$(echo $github_status_output | grep -oE PRAVALER\/\[^#\]+#\[0-9\]\{1,4\}\[^│]+ | sed 's/\(PRAVALER\/[^#]\+#[0-9]\+\)\s\+\(.*$\)/\r\1 <b>\2<\/b>/')"
   alt="pending"
 fi
 
 text=$pr_review_requests_count
-tooltip="<b>Review Requests</b>\r\r"$pr_review_requests
+tooltip="<i>Review Requests</i>\r"$pr_review_requests
 
 print_output
--- waybar/scripts/waybar-khal.py
+++ waybar/scripts/waybar-khal.py
@@ -11,7 +11,7 @@ data = {}
 today = datetime.date.today().strftime("%d/%/%Y")
 
 until_date = (datetime.date.today() +
-          datetime.timedelta(days=4)).strftime("%d/%m/%Y")
+          datetime.timedelta(days=3)).strftime("%d/%m/%Y")
 
 output = subprocess.check_output("khal list now " + until_date + " --format '{start-time}-{end-time} {title}'", shell=True)
 output = output.decode("utf-8")
@@ -23,7 +23,12 @@ if next_event_match == None:
 else:
  data['text'] = "  " + next_event_match.group()
 
-data['tooltip'] = output
+tooltip = escape(output)
+tooltip = "<big><b>Next Events</b></big>\n" + re.sub('(.*, \d{2}/\d{2}/\d{4})', '\n<b>\\1</b>', tooltip)
+
+data['tooltip'] = tooltip
+data['escape'] = True
+
 
 print(json.dumps(data))
 
