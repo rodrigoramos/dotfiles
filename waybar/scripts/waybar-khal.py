6286d505b0d6667219269c5c3fb48afd5e344f73
Rodrigo Ramos Silva <rodrigo.silva@pravaler.com.br>
2023-08-03 14:35:27 -0300

    Fix encoding Khal Message

--- waybar/scripts/waybar-khal.py
+++ waybar/scripts/waybar-khal.py
@@ -21,7 +21,7 @@ next_event_match = re.search("[0-9][0-9]:[0-9][0-9]-[0-9][0-9].+", output)
 if next_event_match == None:
  data['text'] = ""
 else:
- data['text'] = "  " + next_event_match.group()
+ data['text'] = "  " + escape(next_event_match.group())
 
 tooltip = escape(output)
 tooltip = "<big><b>Next Events</b></big>\n" + re.sub('(.*, \d{2}/\d{2}/\d{4})', '\n<b>\\1</b>', tooltip)
